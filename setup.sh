bd67bfda6076038b517ac9e142d4aea25a0a6605
Coenraad Human <coen.human@gmail.com>
2024-04-24 17:01:34 +0200

    feat: gnu sed

--- setup.sh
+++ setup.sh
@@ -4,18 +4,28 @@ echo "Installing GNU coreutils"
 
 brew install coreutils
 
-if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "/opt/homebrew/bin/bash" ]; then
+if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "$HOMEBREW_PREFIX/bin/bash" ]; then
 	echo '' >> ~/.bash_profile
 	echo '# GNU coreutils' >> ~/.bash_profile
-	echo 'export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"' >> ~/.bash_profile
+	echo 'export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"' >> ~/.bash_profile
 fi
 
 echo "Installing GNU findutils"
 
 brew install findutils
 
-if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "/opt/homebrew/bin/bash" ]; then 
+if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "$HOMEBREW_PREFIX/bin/bash" ]; then 
         echo '' >> ~/.bash_profile
         echo '# GNU findutils' >> ~/.bash_profile
-        echo 'export PATH="/opt/homebrew/opt/findutils/libexec/gnubin:$PATH"' >> ~/.bash_profile
+        echo 'export PATH="$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin:$PATH"' >> ~/.bash_profile
+fi
+
+echo "Installing GNU sed"
+
+brew install gnu-sed
+
+if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "$HOMEBREW_PREFIX/bin/bash" ]; then
+        echo '' >> ~/.bash_profile
+        echo '# GNU sed' >> ~/.bash_profile
+        echo 'export PATH="$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin:$PATH"' >> ~/.bash_profile
 fi
